647bd23c16362a8689ca63b1e5d635a3032c08cd
Anastasiya Nikityuk <a.nikityuk@inanomo.com>
2022-03-30 21:04:08 +0300

    read paths from console

--- LargeTextFiles.Sorter/Program.cs
+++ LargeTextFiles.Sorter/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System.Diagnostics;
 using LargeTextFiles.Domain;
 
-var path = @"C:\Users\Home\Documents\FineryMarkets\LargeTextFilesApp\LargeTextFiles.Sorter\bin\Debug\net6.0\100mb.txt";
-var target = @"C:\Users\Home\Documents\FineryMarkets\LargeTextFilesApp\LargeTextFiles.Sorter\bin\Debug\net6.0\outputSorted.txt";
+Console.Write("File to sort: ");
+var path = Console.ReadLine();
+
+Console.Write("Sorted file name: ");
+var target = Console.ReadLine();
 
 var sorter = new Sorter();
 
@@ -17,26 +20,14 @@ if (!File.Exists(path))
 }
 
 target = string.IsNullOrWhiteSpace(target) ? "sorted.txt" : target;
-Process currentProcess = System.Diagnostics.Process.GetCurrentProcess();
-     var stopwatch = new Stopwatch();
-     stopwatch.Start();
+
+Console.Write("Sorting started");
+
+var currentProcess = Process.GetCurrentProcess();
+var stopwatch = new Stopwatch();
+stopwatch.Start();
 await sorter.Sort(path, target);
-     stopwatch.Stop();
-long totalBytesOfMemoryUsed = currentProcess.WorkingSet64;
+stopwatch.Stop();
+var totalBytesOfMemoryUsed = currentProcess.WorkingSet64;
 
 Console.WriteLine($"{stopwatch.ElapsedMilliseconds / 1000d} s elapsed, {totalBytesOfMemoryUsed/1024d/1024d} Mb memory consumed");
-
-//var results = new List<double>();
-// for (var i = 0; i < 1; i++)
-// {
-//     var stopwatch = new Stopwatch();
-//     stopwatch.Start();
-//     await sorter.Sort(new FileStream(path, FileMode.Open), new FileStream(target, FileMode.Create),
-//         CancellationToken.None);
-//     stopwatch.Stop();
-//
-//     var seconds = stopwatch.ElapsedMilliseconds / 1000d;
-//     results.Add(seconds);
-// }
-//
-// Console.WriteLine($"Avg of 1Gb: {results.Average(x => x)}");
